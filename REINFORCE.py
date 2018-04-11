25d52e888eb751a3ac2c8835deee327c6838b2d0
Max LING <wudaoling7@gmail.com>
2018-04-11 00:01:11 -0400

    improve code speed, change training batch

--- REINFORCE.py
+++ REINFORCE.py
@@ -7,16 +7,16 @@ class PolicyEstimator():
     """
         A Policy Function Approximator
     """
-    def __init__(self, state_size, action_size, learn_rate=0.002, scope="policy", layer1_size=5, layer2_size=5):
+    def __init__(self, state_size, action_size, learn_rate=0.001, scope="policy", layer1_size=5, layer2_size=5):
         with tf.variable_scope(scope):
             self.state = tf.placeholder(tf.float32, shape=[None, state_size])
             self.action = tf.placeholder(tf.int32, shape=[None])
             self.target = tf.placeholder(tf.float32, shape=[None])
 
             fc1 = tf.layers.dense(self.state, units=layer1_size, activation=tf.nn.relu)
-            # fc2 = tf.layers.dense(fc1, units=layer2_size, activation=tf.nn.relu)
+            fc2 = tf.layers.dense(fc1, units=layer2_size, activation=tf.nn.relu)
 
-            self.action_softmax = tf.squeeze(tf.nn.softmax(tf.layers.dense(fc1, units=action_size)))
+            self.action_softmax = tf.squeeze(tf.nn.softmax(tf.layers.dense(fc2, units=action_size)))
             action_mask = tf.one_hot(self.action, action_size)
             self.action_pred = tf.reduce_sum(self.action_softmax*action_mask, 1)
 
@@ -27,11 +27,11 @@ class PolicyEstimator():
 
     def predict(self, state, sess=None):
         sess = sess or tf.get_default_session()
-        return sess.run(self.action_softmax, feed_dict={self.state: [state]})
+        return sess.run(self.action_softmax, feed_dict={self.state: state})
 
     def update(self, state, target, action, sess=None):
         sess = sess or tf.get_default_session()
-        feed_dict = {self.state: [state], self.target: [target], self.action: [action]}
+        feed_dict = {self.state: state, self.target: target, self.action: action}
         _, loss = sess.run([self.train, self.loss], feed_dict)
         return loss
 
@@ -40,15 +40,15 @@ class ValueEstimator():
     """
         A Value Function Approximator
     """
-    def __init__(self, state_size, learn_rate=0.002, scope="value", layer1_size=5, layer2_size=5):
+    def __init__(self, state_size, learn_rate=0.001, scope="value", layer1_size=5, layer2_size=5):
         with tf.variable_scope(scope):
             self.state = tf.placeholder(tf.float32, shape=[None, state_size])
             self.target = tf.placeholder(tf.float32, shape=[None])
 
             fc1 = tf.layers.dense(self.state, units=layer1_size, activation=tf.nn.relu)
-            # fc2 = tf.layers.dense(fc1, units=layer2_size, activation=tf.nn.relu)
+            fc2 = tf.layers.dense(fc1, units=layer2_size, activation=tf.nn.relu)
 
-            self.value = tf.squeeze(tf.layers.dense(fc1, units=1))
+            self.value = tf.squeeze(tf.layers.dense(fc2, units=1))
 
             self.loss = tf.square(self.target - self.value)
 
@@ -57,13 +57,14 @@ class ValueEstimator():
 
     def predict(self, state, sess=None):
         sess = sess or tf.get_default_session()
-        return sess.run(self.value, feed_dict={self.state: [state]})
+        return sess.run(self.value, feed_dict={self.state: state})
 
     def update(self, state, target, sess=None):
         sess = sess or tf.get_default_session()
-        feed_dict = {self.state: [state], self.target: [target]}
+        feed_dict = {self.state: state, self.target: target}
         _, loss = sess.run([self.train, self.loss], feed_dict)
         return loss
+
 
 def REINFORCE(env, policy_estimator, value_estimator, iterations, discount = 0.9):
     history = []
@@ -71,25 +72,32 @@ def REINFORCE(env, policy_estimator, value_estimator, iterations, discount = 0.9
         state = env.reset()
         episode = []
         for t in range(env._max_episode_steps):
-            # env.render()
-            action_softmax = policy_estimator.predict(state)
+            if (i+1)%100 == 0:
+                env.render() # render episode
+            action_softmax = policy_estimator.predict(state.reshape((1,-1)))
             action = np.random.choice(np.arange(len(action_softmax)), p=action_softmax)
             next_state, reward, done, _ = env.step(action)
 
-            episode.append([state, action, next_state, reward, done])
             if done:
+                episode.append([state, action, next_state, 1000, done]) # finish reward
                 break
+            episode.append([state, action, next_state, reward, done])
             state = next_state
-        history.append((sum([step[3] for step in episode])/len(episode),t+1))
+        history.append((sum([step[3] for step in episode]),t+1))
         print("Episode " + str(i + 1) + " finished after " +str(t+1)+ " steps")
 
-        for t in range(len(episode)):
-            total_return = sum([discount ** i * r for i, (s, a, s1, r, d) in enumerate(episode[t:])])
-            state, action, next_state, reward, done = episode[t]
-            baseline = value_estimator.predict(state)
-            advantage = total_return - baseline
-            value_estimator.update(state, total_return)
-            policy_estimator.update(state, advantage, action)
+        # training batch: 1 episode
+        states = np.asarray([episode[t][0] for t in range(len(episode))])
+        actions = np.asarray([episode[t][1] for t in range(len(episode))])
+
+        total_returns = np.asarray([sum([discount ** i * r for i, (s, a, s1, r, d) in enumerate(episode[t:])])
+                                    for t in range(len(episode))])
+        baselines = value_estimator.predict(states)
+        advantages = total_returns - baselines
+
+        value_estimator.update(states, total_returns)
+        policy_estimator.update(states, advantages, actions)
+
 
     return history
 
@@ -98,14 +106,16 @@ if __name__ == '__main__':
     # env = gym.make('Acrobot-v1')
     # policy_estimator = PolicyEstimator(state_size=6, action_size=3)
     # value_estimator = ValueEstimator(state_size=6)
+
     # mountain car
     env = gym.make('MountainCar-v0')
+    env._max_episode_steps = 2000
     policy_estimator = PolicyEstimator(state_size=2, action_size=3)
     value_estimator = ValueEstimator(state_size=2)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        history = REINFORCE(env, policy_estimator, value_estimator, iterations=100)
+        history = REINFORCE(env, policy_estimator, value_estimator, iterations=1000)
 
     # smoothing
     window = 10
@@ -114,7 +124,7 @@ if __name__ == '__main__':
     # plot
     fig, (ax1, ax2) = plt.subplots(2, sharex=True)
     ax1.plot(np.arange(0, len(history), window), smooth_history[:,0])
-    ax1.set(title = 'Policy Gradient - MountainCar', ylabel='avg reward')
+    ax1.set(title = 'Policy Gradient - MountainCar', ylabel='reward')
     ax2.plot(np.arange(0, len(history), window), smooth_history[:,1])
     ax2.set(ylabel='length', xlabel='episodes')
-    plt.savefig('pg_mountainCar.png')
+    plt.savefig('pg_MountainCar.png')
